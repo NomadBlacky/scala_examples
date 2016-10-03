bc52132900c4edbf0aded9f536e97add02dab7b8
NomadBlacky <hourou.hamurinzu@gmail.com>
2016-10-03 18:40:07 +0900

    added: TupleSpec.scala

---
+++ src/test/scala/samples/collections/TupleSpec.scala
@@ -0,0 +1,17 @@
+package samples.collections
+
+import org.scalatest.FunSpec
+
+class TupleSpec extends FunSpec {
+  
+  it("タプルを生成する") {
+    val t = (1, "hoge", 1.0)
+  }
+  
+  it("タプルから値を取り出す") {
+    val t = (1, "hoge", 1.0)
+    assert(t._1 == 1)
+    assert(t._2 == "hoge")
+    assert(t._3 == 1.0)
+  }
+}
