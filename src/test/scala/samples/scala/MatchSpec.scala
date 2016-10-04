42c8d4d547d16f187f2828f607c4f9ed4bb07e42
NomadBlacky <hourou.hamurinzu@gmail.com>
2016-10-04 17:26:18 +0900

    added: MatchSpec.scala

---
+++ src/test/scala/samples/scala/MatchSpec.scala
@@ -0,0 +1,41 @@
+package samples.scala
+
+import org.scalatest.FunSpec
+
+/**
+ * match式
+ * 
+ * x match {
+ *   case [選択肢1] => [xが選択肢1にmatchした時の処理]
+ *   case [選択肢2] => [xが選択肢2にmatchした時の処理]
+ *   case _ => [xが選択肢にmatchしなかった時の処理]
+ * }
+ */
+class MatchSpec extends FunSpec {
+  
+  it("基本的なマッチング") {
+     val x = 10
+     x match {
+       case  1 => println("ng")
+       case 10 => println("ok")
+       case  _ => println("ng")
+     }
+  }
+  
+  it("型のマッチング") {
+    val x:Any = "hoge"
+    x match {
+      case i: Int    => println(i)
+      case s: String => println(s)
+      case _         => println("other")
+    }
+  }
+  
+  it("パターンガード") {
+    val x:Any = 50
+    x match {
+      case i: Int if 100 <= i => println(i)
+      case _                  => println("other")
+    }
+  }
+}
