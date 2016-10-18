ff146af5ec4dc4c563b418af6a578e7461587603
NomadBlacky <hourou.hamurinzu@gmail.com>
2016-10-19 00:59:31 +0900

    fixed: FunctionSpec.scala
    * Added placeholder syntax test.

--- src/test/scala/samples/scala/FunctionSpec.scala
+++ src/test/scala/samples/scala/FunctionSpec.scala
@@ -19,4 +19,23 @@ class FunctionSpec extends FunSpec {
     assert(result == 4)
   }
   def calc(f:(Int,Int) => Int, num:Int): Int = f(num, num)
+
+  it("プレースホルダ構文") {
+    // プレースホルダ…
+    // 実際の内容をあとから挿入するために、仮に確保した場所のこと。
+    
+    // 関数の引数の記述を _ で省略できる。
+    val func1:(Int, Int) => Int = (x:Int, y:Int) => x + y
+    val func2:(Int, Int) => Int = _ + _
+    val func3 = (_:Int) + (_:Int)
+    assert(func1(1, 2) == func2(1, 2))
+    assert(func2(1, 2) == func3(1, 2))
+    assert(func1(1, 2) == func3(1, 2))
+  }
+
+  it("プレースホルダ構文2") {
+    val func1:(String) => String = _.replaceAll("hoge", "foo")
+    val func2 = (_:String).replaceAll(_:String, _:String)
+    assert(func1("aaahogebbb") == func2("aaahogebbb", "hoge", "foo"))
+  }
 }
