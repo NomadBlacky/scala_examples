68683ce5c9a56a5163f49bb734c6d06cbbe72b94
NomadBlacky <hourou.hamurinzu@gmail.com>
2016-11-01 22:58:47 +0900

    Add a test of pattern matching to use the 'unapply' method.

--- src/test/scala/samples/scala/ClassSpec.scala
+++ src/test/scala/samples/scala/ClassSpec.scala
@@ -19,4 +19,30 @@ class ClassSpec extends FunSpec{
       new Programmer("")
     }
   }
+
+  it("unapplyを使用したパターンマッチ") {
+    class Designer
+    object Designer {
+      def unapply(arg: Any): Boolean = {
+        if (arg.isInstanceOf[Designer]) true else false
+      }
+    }
+    class Programmer(val language: String)
+    object Programmer {
+      def apply(language: String): Programmer = new Programmer(language)
+      def unapply(arg: Programmer): Option[String] = Some(arg.language)
+    }
+
+    def matchTest(value: Any):String = {
+      value match {
+        case Designer => "designer"
+        case Programmer("scala") => "scala programmer"
+        case _ => "other"
+      }
+    }
+
+    assert(matchTest(Designer) == "designer")
+    assert(matchTest(Programmer("scala")) == "scala programmer")
+    assert(matchTest(Programmer("java")) == "other")
+  }
 }
