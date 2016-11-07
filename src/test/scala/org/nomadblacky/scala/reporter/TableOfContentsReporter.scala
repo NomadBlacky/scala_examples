91042f4a8d83c1721c841f7a1829b3508ccf5d8f
NomadBlacky <hourou.hamurinzu@gmail.com>
2016-11-08 00:32:37 +0900

    Add test to TraitSpec.
    + Case of mixin some same signature methods.
    + Select trait method with 'super' keyword.
    + Usage of trait alone.

--- src/test/scala/org/nomadblacky/scala/reporter/TableOfContentsReporter.scala
+++ src/test/scala/org/nomadblacky/scala/reporter/TableOfContentsReporter.scala
@@ -37,36 +37,36 @@ class TableOfContentsReporter() extends Reporter {
       case TestPending(ordinal, suiteName, suiteId, suiteClassName, testName, testText, recordedEvents, duration, formatter, location, payload, threadName, timeStamp) =>
         stringBuilder.append("+ " + testName + " (Pending)\n")
 
-      case e:RunCompleted =>
+      case e: RunCompleted =>
         val pw = new PrintWriter(markdownFilePath.toFile)
         pw.println(stringBuilder.toString())
         pw.close()
 
-//      case _: RecordableEvent =>
-//      case _: ExceptionalEvent =>
-//      case _: NotificationEvent =>
-//      case TestStarting(ordinal, suiteName, suiteId, suiteClassName, testName, testText, formatter, location, rerunner, payload, threadName, timeStamp) =>
-//      case TestSucceeded(ordinal, suiteName, suiteId, suiteClassName, testName, testText, recordedEvents, duration, formatter, location, rerunner, payload, threadName, timeStamp) =>
-//      case TestFailed(ordinal, message, suiteName, suiteId, suiteClassName, testName, testText, recordedEvents, throwable, duration, formatter, location, rerunner, payload, threadName, timeStamp) =>
-//      case TestIgnored(ordinal, suiteName, suiteId, suiteClassName, testName, testText, formatter, location, payload, threadName, timeStamp) =>
-//      case TestPending(ordinal, suiteName, suiteId, suiteClassName, testName, testText, recordedEvents, duration, formatter, location, payload, threadName, timeStamp) =>
-//      case TestCanceled(ordinal, message, suiteName, suiteId, suiteClassName, testName, testText, recordedEvents, throwable, duration, formatter, location, rerunner, payload, threadName, timeStamp) =>
-//      case SuiteStarting(ordinal, suiteName, suiteId, suiteClassName, formatter, location, rerunner, payload, threadName, timeStamp) =>
-//      case SuiteCompleted(ordinal, suiteName, suiteId, suiteClassName, duration, formatter, location, rerunner, payload, threadName, timeStamp) =>
-//      case SuiteAborted(ordinal, message, suiteName, suiteId, suiteClassName, throwable, duration, formatter, location, rerunner, payload, threadName, timeStamp) =>
-//      case RunStarting(ordinal, testCount, configMap, formatter, location, payload, threadName, timeStamp) =>
-//      case RunCompleted(ordinal, duration, summary, formatter, location, payload, threadName, timeStamp) =>
-//      case RunStopped(ordinal, duration, summary, formatter, location, payload, threadName, timeStamp) =>
-//      case RunAborted(ordinal, message, throwable, duration, summary, formatter, location, payload, threadName, timeStamp) =>
-//      case InfoProvided(ordinal, message, nameInfo, throwable, formatter, location, payload, threadName, timeStamp) =>
-//      case AlertProvided(ordinal, message, nameInfo, throwable, formatter, location, payload, threadName, timeStamp) =>
-//      case NoteProvided(ordinal, message, nameInfo, throwable, formatter, location, payload, threadName, timeStamp) =>
-//      case MarkupProvided(ordinal, text, nameInfo, formatter, location, payload, threadName, timeStamp) =>
-//      case ScopeOpened(ordinal, message, nameInfo, formatter, location, payload, threadName, timeStamp) =>
-//      case ScopeClosed(ordinal, message, nameInfo, formatter, location, payload, threadName, timeStamp) =>
-//      case ScopePending(ordinal, message, nameInfo, formatter, location, payload, threadName, timeStamp) =>
-//      case DiscoveryStarting(ordinal, configMap, threadName, timeStamp) =>
-//      case DiscoveryCompleted(ordinal, duration, threadName, timeStamp) =>
+      //      case _: RecordableEvent =>
+      //      case _: ExceptionalEvent =>
+      //      case _: NotificationEvent =>
+      //      case TestStarting(ordinal, suiteName, suiteId, suiteClassName, testName, testText, formatter, location, rerunner, payload, threadName, timeStamp) =>
+      //      case TestSucceeded(ordinal, suiteName, suiteId, suiteClassName, testName, testText, recordedEvents, duration, formatter, location, rerunner, payload, threadName, timeStamp) =>
+      //      case TestFailed(ordinal, message, suiteName, suiteId, suiteClassName, testName, testText, recordedEvents, throwable, duration, formatter, location, rerunner, payload, threadName, timeStamp) =>
+      //      case TestIgnored(ordinal, suiteName, suiteId, suiteClassName, testName, testText, formatter, location, payload, threadName, timeStamp) =>
+      //      case TestPending(ordinal, suiteName, suiteId, suiteClassName, testName, testText, recordedEvents, duration, formatter, location, payload, threadName, timeStamp) =>
+      //      case TestCanceled(ordinal, message, suiteName, suiteId, suiteClassName, testName, testText, recordedEvents, throwable, duration, formatter, location, rerunner, payload, threadName, timeStamp) =>
+      //      case SuiteStarting(ordinal, suiteName, suiteId, suiteClassName, formatter, location, rerunner, payload, threadName, timeStamp) =>
+      //      case SuiteCompleted(ordinal, suiteName, suiteId, suiteClassName, duration, formatter, location, rerunner, payload, threadName, timeStamp) =>
+      //      case SuiteAborted(ordinal, message, suiteName, suiteId, suiteClassName, throwable, duration, formatter, location, rerunner, payload, threadName, timeStamp) =>
+      //      case RunStarting(ordinal, testCount, configMap, formatter, location, payload, threadName, timeStamp) =>
+      //      case RunCompleted(ordinal, duration, summary, formatter, location, payload, threadName, timeStamp) =>
+      //      case RunStopped(ordinal, duration, summary, formatter, location, payload, threadName, timeStamp) =>
+      //      case RunAborted(ordinal, message, throwable, duration, summary, formatter, location, payload, threadName, timeStamp) =>
+      //      case InfoProvided(ordinal, message, nameInfo, throwable, formatter, location, payload, threadName, timeStamp) =>
+      //      case AlertProvided(ordinal, message, nameInfo, throwable, formatter, location, payload, threadName, timeStamp) =>
+      //      case NoteProvided(ordinal, message, nameInfo, throwable, formatter, location, payload, threadName, timeStamp) =>
+      //      case MarkupProvided(ordinal, text, nameInfo, formatter, location, payload, threadName, timeStamp) =>
+      //      case ScopeOpened(ordinal, message, nameInfo, formatter, location, payload, threadName, timeStamp) =>
+      //      case ScopeClosed(ordinal, message, nameInfo, formatter, location, payload, threadName, timeStamp) =>
+      //      case ScopePending(ordinal, message, nameInfo, formatter, location, payload, threadName, timeStamp) =>
+      //      case DiscoveryStarting(ordinal, configMap, threadName, timeStamp) =>
+      //      case DiscoveryCompleted(ordinal, duration, threadName, timeStamp) =>
       case _ => // Noting to do.
     }
   }
--- src/test/scala/org/nomadblacky/scala/samples/scala/TraitSpec.scala
+++ src/test/scala/org/nomadblacky/scala/samples/scala/TraitSpec.scala
@@ -14,18 +14,18 @@ class TraitSpec extends FunSpec{
 
   it("トレイトの基本") {
     trait Programmer {
-      def coding: String = "I'm writing program code."
+      def coding: String = "I'm writing a program code."
     }
 
     // クラスが明示的に継承をしない場合、 extends キーワードでミックスインする
     class Person(val name:String) extends Programmer
 
-    assert(new Person("Tom").coding == "I'm writing program code.")
+    assert(new Person("Tom").coding == "I'm writing a program code.")
   }
 
   it("複数のトレイトをミックスインする") {
     trait Programmer {
-      def coding:String = "I'm writing program code."
+      def coding:String = "I'm writing a program code."
     }
     trait Designer {
       def design:String = "I'm making a design."
@@ -35,18 +35,71 @@ class TraitSpec extends FunSpec{
     class Person(val name:String) extends Programmer with Designer
     val p = new Person("Alex")
 
-    assert(p.coding == "I'm writing program code.")
+    assert(p.coding == "I'm writing a program code.")
     assert(p.design == "I'm making a design.")
   }
 
   it("インスタンス化のタイミングでミックスインする") {
     trait Programmer {
-      def coding:String = "I'm writing program code."
+      def coding:String = "I'm writing a program code."
     }
     class Person(val name:String)
 
     val p = new Person("Tom") with Programmer
 
-    assert(p.coding == "I'm writing program code.")
+    assert(p.coding == "I'm writing a program code.")
+  }
+
+  it("同じシグネチャのメソッドを複数ミックスインした場合") {
+    trait Programmer {
+      def write:String = "I'm writing a program code."
+    }
+    trait Writer {
+      def write:String = "I'm writing a blog."
+    }
+
+    class Person extends Programmer with Writer {
+      // メソッドを必ずオーバーライドしなくてはならない。(エラーになる。)
+      override def write:String = "I'm writing a document."
+    }
+  }
+
+  it("superで呼び出すトレイトのメソッドを指定する") {
+    trait Programmer {
+      def write:String = "I'm writing a program code."
+    }
+    trait Writer {
+      def write:String = "I'm writing a blog."
+    }
+    class Person
+
+    val p1 = new Person with Programmer with Writer {
+      override def write:String = super[Programmer].write
+    }
+    val p2 = new Person with Programmer with Writer {
+      override def write:String = super[Writer].write
+    }
+
+    assert(p1.write == "I'm writing a program code.")
+    assert(p2.write == "I'm writing a blog.")
+  }
+
+  it("トレイトを単体で利用する") {
+    trait T1 {
+      def method1:String = "You are calling method1."
+    }
+    trait T2 {
+      def method2:String = "You are calling method2."
+    }
+
+    // {} はトレイトをミックスインした無名クラスを作成することを意味している。
+    val instance1 = new T1 {}
+    assert(instance1.method1 == "You are calling method1.")
+
+    // 複数のトレイトをミックスインした無名クラスを作成できる。
+    val instance2 = new T1 with T2 // この場合の {} は不要
+
+    assert(instance2.method1 == "You are calling method1.")
+    assert(instance2.method2 == "You are calling method2.")
   }
 }
