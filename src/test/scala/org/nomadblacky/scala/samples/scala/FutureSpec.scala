ed12fbdb0a0c1a21e022f5846d5e92b21175b6d7
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-04-01 23:49:18 +0900

    Add a test about using Await to FutureSpec

--- src/test/scala/org/nomadblacky/scala/samples/scala/FutureSpec.scala
+++ src/test/scala/org/nomadblacky/scala/samples/scala/FutureSpec.scala
@@ -4,6 +4,7 @@ import org.scalatest.FunSpec
 
 import scala.concurrent._
 import ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success}
 
 /**
@@ -37,4 +38,24 @@ class FutureSpec extends FunSpec {
     assert(f.value.get.get == "ok")
   }
 
+  it("Await ... スレッドの終了を待機する") {
+    val f = Future {
+      Thread.sleep(1000)
+      "ok"
+    }
+    // Await.result でスレッドの終了を待機して結果を受け取る
+    val result = Await.result(f, Duration.Inf)
+    assert(result == "ok")
+
+    val f2: Future[String] = Future {
+      Thread.sleep(1000)
+      throw new RuntimeException()
+    }
+    // Futureの例外を受け取りたい場合は Await.ready で処理を終えてからFutureのメソッドで結果を受け取る
+    Await.ready(f2, Duration.Inf)
+    f2.value.get match {
+      case Success(_) => fail()
+      case Failure(ex) => assert(ex.isInstanceOf[RuntimeException])
+    }
+  }
 }
