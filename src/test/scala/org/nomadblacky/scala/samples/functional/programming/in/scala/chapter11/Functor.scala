0013921dfad87ab0b9872fb56a5b39dac4e220e2
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-04-25 22:34:25 +0900

    Add a test class about "Functional Programming in Scala" Chapter11

---
+++ src/test/scala/org/nomadblacky/scala/samples/functional/programming/in/scala/chapter11/Functor.scala
@@ -0,0 +1,25 @@
+package org.nomadblacky.scala.samples.functional.programming.in.scala.chapter11
+
+/**
+  * Created by blacky on 17/04/25.
+  *
+  * Functorは全体を移せる(map over)ものの型クラス。
+  * List, Optionなどの型コンストラクタなど。
+  * Functor[F]インスタンスは、Fが実際にファンクタであることの裏付けとなる。
+  */
+trait Functor[F[_]] {
+  // map を実装する
+  def map[A,B](fa: F[A])(f: A => B): F[B]
+
+  // mapのみで定義できる演算の一例
+  def distribute[A,B](fab: F[(A, B)]): (F[A], F[B]) = {
+    (map(fab)(_._1), map(fab)(_._2))
+  }
+
+  def codistribute[A,B](e: Either[F[A], F[B]]): F[Either[A,B]] = {
+    e match {
+      case Left(fa) => map(fa)(Left(_))
+      case Right(fb) => map(fb)(Right(_))
+    }
+  }
+}
