a35b04c1b5d00e4626f1af237c0d28ea5aa86e4a
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-09-02 19:11:45 +0900

    Fix to use matchers in OptionSpec.scala

--- src/test/scala/org/nomadblacky/scala/samples/exceptions/OptionSpec.scala
+++ src/test/scala/org/nomadblacky/scala/samples/exceptions/OptionSpec.scala
@@ -1,24 +1,25 @@
 package org.nomadblacky.scala.samples.exceptions
 
-import org.scalatest.FunSpec
+import org.scalatest.{FunSpec, Matchers}
 
 /**
   * Created by blacky on 16/12/04.
   */
-class OptionSpec extends FunSpec {
+class OptionSpec extends FunSpec with Matchers {
 
   it("Optionの基本") {
     // Option[+A] ... 値が存在しない可能性があることを表すクラス
 
     // Some の場合は値が存在し、
     val o1:Option[Int] = Some(1)
+
     // None の場合は値が存在しない
     val o2:Option[Int] = None
 
-    assert(o1.get == 1)
-    assertThrows[NoSuchElementException] {
-      o2.get
-    }
+    o1.get shouldBe 1
+
+    // Noneのときにgetするとエラー
+    assertThrows[NoSuchElementException](o2.get)
   }
 
   it("Optionから値を取り出す") {
@@ -27,7 +28,7 @@ class OptionSpec extends FunSpec {
 
     // 主にmatch式や、
     o1 match {
-      case Some(i) => assert(i == 1)
+      case Some(i) => i shouldBe 1
       case _ => fail()
     }
     o2 match {
@@ -36,8 +37,8 @@ class OptionSpec extends FunSpec {
     }
 
     // getOrElseを使う
-    assert(o1.getOrElse(2) == 1)
-    assert(o2.getOrElse(2) == 2)
+    o1.getOrElse(2) shouldBe 1
+    o2.getOrElse(2) shouldBe 2
   }
 
   it("foreach ... Optionに値が含まれる場合のみに実行させる") {
@@ -45,8 +46,8 @@ class OptionSpec extends FunSpec {
     val o2 = None
 
     // foreach を使うと、Someの場合のみに実行させるといったことができる
-    // Optionは要素数1のリストというイメージ
-    o1 foreach { i => assert(i == 1) }
+    // 要素数1のリストとイメージすると foreach という命名がわかりやすい
+    o1 foreach { i => i shouldBe 1 }
     o2 foreach { _ => fail() }
   }
 
@@ -54,17 +55,17 @@ class OptionSpec extends FunSpec {
     val o1: Option[Int] = Some(1)
     val o2: Option[Int] = None
 
-    assert(o1.map(_ + 10) == Some(11))
-    assert(o2.map(_ + 10) == None)
+    o1.map(_ + 10) shouldBe Some(11)
+    o2.map(_ + 10) shouldBe None
   }
 
   it("flatMap ... 中身の値を関数に適用し、SomeならSomeを、NoneならNoneを返す") {
     val o1: Option[Int] = Some(1)
     val o2: Option[Int] = None
 
-    assert(o1.flatMap(i => Some(i + 10)) == Some(11))
-    assert(o1.flatMap(_ => None) == None)
-    assert(o2.flatMap(i => Some(i + 10)) == None)
-    assert(o2.flatMap(_ => None) == None)
+    o1.flatMap(i => Some(i + 10)) shouldBe Some(11)
+    o1.flatMap(_ => None)         shouldBe None
+    o2.flatMap(i => Some(i + 10)) shouldBe None
+    o2.flatMap(_ => None)         shouldBe None
   }
 }
