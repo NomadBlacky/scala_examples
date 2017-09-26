c383bdda3f81dbf94904de3ac06f882dab107607
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-09-27 00:12:34 +0900

    :thinking_face:

--- update-readme.sh
+++ update-readme.sh
@@ -5,6 +5,6 @@ if git status -s | grep README.md; then
     git config user.name "travis"
     git add -u
     git commit -m "Update README.md"
-    git push -q "https://${GH_TOKEN}@github.com/NomadBlacky/scala_samples.git" master:master
+    git push -fq "https://${GH_TOKEN}@github.com/NomadBlacky/scala_samples.git" master:master
     echo "OK: git push"
 fi
