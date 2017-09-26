4c60e3241e36d1822b3301d3a0156d7436873fee
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-09-27 00:06:08 +0900

    Fix Travis CI tasks

--- update-readme.sh
+++ update-readme.sh
@@ -5,5 +5,6 @@ if git status -s | grep README.md; then
     git config user.name "travis"
     git add -u
     git commit -m "Update README.md"
-    git push "https://${GH_TOKEN}@github.com/NomadBlacky/scala-samples.git" master:master
+    git push -q "https://${GH_TOKEN}@github.com/NomadBlacky/scala-samples.git" master:master
+    echo "OK: git push"
 fi
