54e9b6f4ce0fe00aa83f347ca2bd7c820049ace9
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-02-27 23:51:13 +0900

    Add: DynamicSpec

---
+++ src/test/scala/org/nomadblacky/scala/samples/scala/DynamicSpec.scala
@@ -0,0 +1,27 @@
+package org.nomadblacky.scala.samples.scala
+
+import org.scalatest.{FunSpec, Matchers}
+
+/**
+  * scala.Dynamic
+  * 特殊な構文を提供するトレイト
+  */
+class DynamicSpec extends FunSpec with Matchers {
+
+  override def suiteName: String = "scala.Dynamic ... 動的言語のような構文をサポートする"
+
+  // Dynamicの構文を利用するのに必要
+  import scala.language.dynamics
+
+  it("applyDynamic") {
+    case class MyMap[V](m: Map[String, V]) extends Dynamic {
+      def applyDynamic(key: String)(default: V): V = m.getOrElse(key, default)
+    }
+    val mymap = MyMap(Map("aaa" -> 10))
+
+    // フィールドに対してapplyしているような構文を書ける
+    mymap.aaa(-1) shouldBe 10
+    mymap.bbb(-1) shouldBe -1
+  }
+
+}
