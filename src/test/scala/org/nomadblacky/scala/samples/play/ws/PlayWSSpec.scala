6beb1a89c9536db271a8afe3084e8ade94a09732
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-03-06 20:59:57 +0900

    Add a test to PlayWSSpec

--- src/test/scala/org/nomadblacky/scala/samples/play/ws/PlayWSSpec.scala
+++ src/test/scala/org/nomadblacky/scala/samples/play/ws/PlayWSSpec.scala
@@ -2,11 +2,14 @@ package org.nomadblacky.scala.samples.play.ws
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Keep, Sink}
+import akka.util.ByteString
 import org.scalatest.{FunSpec, Matchers}
 import play.api.libs.ws.StandaloneWSRequest
 import play.api.libs.ws.ahc.StandaloneAhcWSClient
 
-import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 
 class PlayWSSpec extends FunSpec with Matchers {
@@ -27,4 +30,14 @@ class PlayWSSpec extends FunSpec with Matchers {
     }
   }
 
+  it("akka-streamsのSourceとして受け取る") {
+    val client = StandaloneAhcWSClient()
+    val flow = Flow[ByteString].filter(bs => bs.utf8String contains "G")
+    val sink = Sink.fold[Int, ByteString](0)((acc, bs) => acc + bs.length)
+    val future = client.url("https://google.co.jp").stream().flatMap { res =>
+      res.bodyAsSource.via(flow).toMat(sink)(Keep.right).run()
+    }
+    val result = Await.result(future, Duration.Inf)
+    println(result)
+  }
 }
