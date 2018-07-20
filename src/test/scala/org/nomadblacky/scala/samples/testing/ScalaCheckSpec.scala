dcaa682dd2d5faf256738288a7855d6210155721
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-07-21 00:28:18 +0900

    Add tests to ScalaCheckSpec

--- src/test/scala/org/nomadblacky/scala/samples/testing/ScalaCheckSpec.scala
+++ src/test/scala/org/nomadblacky/scala/samples/testing/ScalaCheckSpec.scala
@@ -25,17 +25,51 @@ class ScalaCheckSpec extends FunSpec with Matchers with GeneratorDrivenPropertyC
   }
 
   it("Gen ... 値を生成するジェネレータ") {
+    // 1,2,3 の中からひとつランダムに生成されるGen
+    val intGen: Gen[Int] = Gen.oneOf(1, 2, 3)
+
+    forAll(intGen) { i =>
+      (1 <= i && i <= 3) shouldBe true
+    }
+
+    // for式でGenを組み合わせられる
     val userGen: Gen[User] = for {
-      name <- Gen.alphaNumStr
-      age  <- Gen.choose(1, 100)
+      name <- Gen.oneOf("a", "b", "c")
+      age  <- Gen.oneOf(10, 20, 30)
     } yield User(name, age)
 
     forAll(userGen) { (u: User) =>
-      (1 <= u.age && u.age <= 100) shouldBe true
+      (u.name + u.age.toString).length shouldBe 3
     }
   }
 
-  it("suchThat ... ジェネレータに対するフィルタ") {
+  it("Gen.oneOf ... 複数の要素からひとつを選択する") {
+    val stringGen = Gen.oneOf("hoge", "foo", "bar")
+    forAll(stringGen)(s => s.toLowerCase shouldEqual s)
+  }
+
+  it("Gen.someOf ... 複数の要素から0個以上選択する") {
+    val stringListGen = Gen.someOf(Seq("a", "b", "c"))
+    forAll(stringListGen)(ss => ss.mkString shouldBe ss.fold("")(_ + _))
+  }
+
+  it("Gen.choose ... 範囲の中からひとつ選択する") {
+    val intGen = Gen.choose(-100, 100)
+    forAll(intGen)(i => i * 2 shouldEqual i + i)
+  }
+
+  it("Gen.alphaNumStr ... ランダムな文字列を生成する") {
+    val stringGen = Gen.alphaNumStr
+    forAll(stringGen)(_.contains("@") shouldEqual false)
+
+    // そのほかのバリエーション
+    forAll(Gen.alphaStr)(_.contains("0") shouldEqual false)
+    forAll(Gen.alphaLowerStr)(_.contains("A") shouldEqual false)
+    forAll(Gen.alphaUpperStr)(_.contains("a") shouldEqual false)
+    forAll(Gen.numStr)(s => (s.contains("a") || s.contains("A")) shouldEqual false)
+  }
+
+  it("Gen#suchThat ... ジェネレータに対するフィルタ") {
     val userGen = for {
       name <- Gen.alphaNumStr
       age  <- Gen.choose(1, 100).suchThat(20 <= _)
