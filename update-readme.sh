ce0bc46bd3ba4bbb20f13ea0aa9aaa2c73defdec
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-07-21 14:35:21 +0900

    Fix the commit message of when update README

--- update-readme.sh
+++ update-readme.sh
@@ -5,7 +5,7 @@ if git status -s | grep README.md; then
     git config user.email "travis@travis.com"
     git config user.name "travis"
     git add -u
-    git commit -m "Update README.md"
+    git commit -m "Update table of contents"
     git push -fq "https://${GH_TOKEN}@github.com/NomadBlacky/scala_samples.git" master:master
     echo "OK: git push"
 fi
